cf05cf8fe06a6010e85e4381ec4ffe3db2b3afac
Curtis Bridge <c.bridge0496@gmail.com>
2021-09-30 16:30:21 -0400

    heroku not pushing test

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,37 @@
+
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <div>
+                    <p className='copy'>&copy; Copyright 2021, curtis-and-kenny</p>
+                </div>
+                <div className="about-us__footer">
+                    <div>
+                        <p>Curtis Bridge</p>
+                        <a href='https://github.com/C-Bridge17' target='blank'>
+                            <i className="fab fa-github-square icon"></i>
+                        </a>
+                        {/* Update This Link to LinkedIn */}
+                        <a href='https://www.linkedin.com/in/curtis-bridge-b126b121a/' target='blank'>
+                            <i className="fab fa-linkedin icon"></i>
+                        </a>
+                    </div>
+                    <div>
+                        <p>Kenneth Donahue</p>
+                        <a href='https://github.com/veryKenny' target='blank'>
+                            <i className="fab fa-github-square icon"></i>
+                        </a>
+                        <a href='https://www.linkedin.com/in/kenneth-donahue/' target='blank'>
+                            <i className="fab fa-linkedin icon"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
